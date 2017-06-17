6d29ec9667f9519d25a90d73b36e6e920e4d012e
Amrit Sultani <amritsultani@users.noreply.github.com>
2017-06-17 23:31:38 +0100

    included dictionaries
    the dictionaries sorted and displayed the low calories drinks

--- mcdcal.py
+++ mcdcal.py
@@ -26,7 +26,7 @@ for c in cat_slice:
     else: 
         category_counts[c] = 1
 
-print(category_counts)
+print('\n',category_counts,'\n')
 
 calories = []
 for r in final_data:
@@ -50,5 +50,23 @@ for cal in cal_int:
     else:
         low_cal.append(cal)
 
-print(high_cal)
+# print(high_cal)
 print(low_cal)
+
+items = []
+for i in final_data:
+    item = i[1]
+    items.append(item)
+item_slice = items[1:len(items)-1]
+
+item_cal = dict(zip(item_slice, cal_int)) # pairs items and calories in a dictionary
+# print(item_cal)
+
+hlatte = item_cal["Hazelnut Latte (Large)"]
+print("A Large Hazelnut Latte has",hlatte, "calories",'\n')
+
+low_items = [k for k,v in item_cal.items() if v <= 150] # these items are less than 150 calories each
+print("LOW CALORIE DRINKS",*low_items, sep='\n') # seperates each item per line
+
+
+
